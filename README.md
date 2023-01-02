d2c7b8a9078a7b6ab2cb1215669c7471d654017e
QuocMaChe <quocmache@gmail.com>
2023-01-02 23:58:59 +0700

    up database

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+Tạo database ở trong file Ticket.sql
+
 B1: npm install (để tải thư viện)
 B2: npm start (chạy app)
 B3: vào chrome vào URL gõ --> localhost:1111
---
+++ Ticket.sql
@@ -0,0 +1,30 @@
+﻿use DOAN_NMCNPM
+go
+
+CREATE
+--ALTER
+TABLE KHACHHANG(
+	ID INT IDENTITY(1,1) NOT NULL PRIMARY KEY,
+	TEN NVARCHAR(100) NOT NULL,
+	EMAIL CHAR(100) NOT NULL,
+	PWD CHAR(100) NOT NULL,
+)
+
+CREATE
+--ALTER
+TABLE PHIM(
+	ID INT IDENTITY(1,1) NOT NULL PRIMARY KEY,
+	TEN NVARCHAR(100) NOT NULL,
+	MOTA CHAR(100) NOT NULL,
+	THOILUONG CHAR(100) NOT NULL,
+
+)
+
+go
+insert into KHACHHANG values(N'Nguyễn Quốc Anh','zp19d0z@gmail.com','1234');
+insert into KHACHHANG values(N'Nguyễn Quốc Vinh','quocmache@gmail.com','1234');
+insert into KHACHHANG values(N'Nguyễn Quốc Vũ','quocanhnguyen@gmail.com','1234');
+insert into KHACHHANG values(N'Nguyễn Quốc Tùng','quocanhnguyen1@gmail.com','1234');
+insert into KHACHHANG values(N'Nguyễn Quốc Huy','quocanhnguyen2@gmail.com','1234');
+delete KHACHHANG where ID=8
+select * from KHACHHANG where EMAIL='zp19d0z@gmail.com' and PWD ='1234'
